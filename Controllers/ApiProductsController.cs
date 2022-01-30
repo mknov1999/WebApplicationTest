6d50df015126d6b62630e5b78bad8d543d27f47c
mknov1999 <mknov1999@mail.ru>
2022-01-30 13:43:41 +0300

    Добавьте файлы проекта.

---
+++ Controllers/ApiProductsController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApplicationTest.Models;
+
+namespace WebApplicationTest.Controllers
+{
+    public class ApiProductsController : ApiController
+    {
+        private TestDbEntities db = new TestDbEntities();
+
+        
+
+        public IEnumerable<Product> GetAll()
+        {
+            return db.Products;
+        }
+
+        [ResponseType(typeof(Product))]
+        public IHttpActionResult Get(Guid id)
+        {
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+
+        [ResponseType(typeof(void))]
+        public IHttpActionResult Put(Product product)
+        {
+
+            var updateProduct = db.Products.Where(x=> x.ID == product.ID).FirstOrDefault<Product>();
+            if (updateProduct != null)
+            {
+                updateProduct.Name = product.Name;
+                updateProduct.Description = product.Description;
+                db.SaveChanges();
+            }
+            else
+            {
+                return NotFound();
+            }
+
+            return Ok();
+        }
+
+        [ResponseType(typeof(Product))]
+        public IHttpActionResult Post(Product product)
+        {
+            product.ID = Guid.NewGuid();
+            db.Products.Add(product);
+            db.SaveChanges();
+
+            return Ok();
+        }
+
+        [ResponseType(typeof(Product))]
+        public IHttpActionResult Delete(Guid id)
+        {
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            db.Products.Remove(product);
+            db.SaveChanges();
+
+            return Ok(product);
+        }
+    }
+}
---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using WebApplicationTest.Models;
+
+namespace WebApplicationTest.Controllers
+{
+    public class ProductsController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            IEnumerable<Product> products = null;
+            HttpClient httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("https://localhost:44306/api/ApiProducts");
+
+            var productapi = httpClient.GetAsync("ApiProducts");
+            productapi.Wait();
+
+            var result = productapi.Result;
+            if (result.IsSuccessStatusCode)
+            {
+                var displayData = result.Content.ReadAsAsync<IList<Product>>();
+                displayData.Wait();
+
+                products = displayData.Result;
+            }
+            return View(products);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            HttpClient httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("https://localhost:44306/api/ApiProducts");
+
+            var productRecord = httpClient.PostAsJsonAsync<Product>("ApiProducts", product);
+            productRecord.Wait();
+
+            var result = productRecord.Result;
+            if (result.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View("Create");
+        }
+
+        public ActionResult Details(Guid id)
+        {
+            Product product = null;
+
+            HttpClient http = new HttpClient();
+            http.BaseAddress = new Uri("https://localhost:44306/api/");
+
+            var productApi = http.GetAsync("ApiProducts?id=" + id.ToString());
+            productApi.Wait();
+
+            var result = productApi.Result;
+            if (result.IsSuccessStatusCode)
+            {
+                var displayData = result.Content.ReadAsAsync<Product>();
+                displayData.Wait();
+                product = displayData.Result;
+            }
+            return View(product);
+        }
+
+        public ActionResult Edit(Guid id)
+        {
+            Product product = null;
+
+            HttpClient http = new HttpClient();
+            http.BaseAddress = new Uri("https://localhost:44306/api/");
+
+            var productApi = http.GetAsync("ApiProducts?id=" + id.ToString());
+            productApi.Wait();
+
+            var result = productApi.Result;
+            if (result.IsSuccessStatusCode)
+            {
+                var displayData = result.Content.ReadAsAsync<Product>();
+                displayData.Wait();
+                product = displayData.Result;
+            }
+            return View(product);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Product product)
+        {
+            HttpClient http = new HttpClient();
+            http.BaseAddress = new Uri("https://localhost:44306/api/ApiProducts");
+
+            var productRecord = http.PutAsJsonAsync<Product>("ApiProducts", product);
+            productRecord.Wait();
+
+            var result = productRecord.Result;
+            if (result.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.message = "Product Record Not Updates  ... !";
+            }
+            return View(product);
+        }
+
+        public ActionResult Delete(Guid id)
+        {
+            HttpClient http = new HttpClient();
+            http.BaseAddress = new Uri("https://localhost:44306/api/ApiProducts");
+
+            var delRecord = http.DeleteAsync("ApiProducts/" + id.ToString());
+            delRecord.Wait();
+
+            var result = delRecord.Result;
+            if (result.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View("Index");
+        }
+    }
+}
